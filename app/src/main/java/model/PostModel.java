c8f863e197c57cf97c11b7acaf0581e7728ba24a
Keeras <keerasmhz@gmail.com>
2020-01-17 02:06:21 +0545

    files added after

--- app/src/main/java/model/PostModel.java
+++ app/src/main/java/model/PostModel.java
@@ -1,11 +1,13 @@
 package model;
 
 public class PostModel {
-    private String caption, postImage;
+    private String caption, postImage, username, userImage;
 
-    public PostModel(String caption, String postImage) {
+    public PostModel(String caption, String postImage, String username, String userImage) {
         this.caption = caption;
         this.postImage = postImage;
+        this.username = username;
+        this.userImage = userImage;
     }
 
     public String getCaption() {
@@ -22,5 +24,21 @@ public class PostModel {
 
     public void setPostImage(String postImage) {
         this.postImage = postImage;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(String userImage) {
+        this.userImage = userImage;
     }
 }
--- app/src/main/java/model/Response.java
+++ app/src/main/java/model/Response.java
@@ -1,8 +1,24 @@
 package model;
 
 public class Response {
-    private String message, status;
+    private String username, message, status, userImage;
     private boolean success;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(String userImage) {
+        this.userImage = userImage;
+    }
 
     public String getMessage() {
         return message;
